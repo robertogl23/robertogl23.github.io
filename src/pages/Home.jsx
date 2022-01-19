12fca72d51ecd04021b2b945d0bf27d0e7b2715f
Roberto Carlos Galicia Lopez <dev.robertgl@gmail.com>
2022-01-19 15:14:09 -0600

    Arreglando un poco el diseño

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,15 @@
 import { Globe } from "../components/Globe";
+import styles from "../styles/home.module.css";
 
 export function Home() {
   return (
-    <main className="rob-page">
+    <main className={styles["main-layout"]}>
       <header>
         <div className="title-layout">
+          <h1>Hola soy roberto</h1>
           <p>Desarrollador web</p>
         </div>
-        <Globe />
+        {/* <Globe /> */}
       </header>
     </main>
   );
